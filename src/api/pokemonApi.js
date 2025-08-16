015680a7a814f78eda44dd34ecd11c8ff3ba8744
Juan <juanjluarca@gmail.com>
2025-08-16 11:42:47 -0600

    feat: creating the connection with PokeApi

---
+++ src/api/pokemonApi.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+export const getPokemonList = async (limit = 151, offset = 0) => {
+    try {
+        const res = await axios.get(`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`);
+        return res.data.results;
+    } catch (err) {
+        console.error("Error fetching Pokémon list:", err);
+        return [];
+    }
+};
+
+export const getPokemonDetail = async (url) => {
+    try {
+        const res = await axios.get(url);
+        return {
+            id: res.data.id,
+            name: res.data.name,
+            image: res.data.sprites.other["official-artwork"].front_default,
+            types: res.data.types.map((t) => t.type.name),
+        };
+    } catch (err) {
+        console.error("Error fetching Pokemon detail:", err);
+        return null;
+    }
+};
+
+export const getPokemonsWithDetails = async (limit = 151, offset = 0) => {
+    const list = await getPokemonList(limit, offset);
+    const detailedData = await Promise.all(
+        list.map(async (p) => await getPokemonDetail(p.url))
+    );
+    return detailedData;
+};
