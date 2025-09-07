7f676664d4e44ec9a52137751f1af39aae61261b
Juan <juanjluarca@gmail.com>
2025-09-07 14:36:35 -0600

    feat: creating country model

--- library/models.py
+++ library/models.py
@@ -6,4 +6,10 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     birth_date = models.DateField()
-    
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    population = models.PositiveBigIntegerField()
+    territorial_area = models.FloatField()
+
