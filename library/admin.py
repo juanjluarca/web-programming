374550d3efdd765359fe3ddbc943d57119a10161
Juan <juanjluarca@gmail.com>
2025-09-07 14:43:00 -0600

    feat: adding default value to Country table

--- library/admin.py
+++ library/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Author
+from .models import Country
 # Register your models here.
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
