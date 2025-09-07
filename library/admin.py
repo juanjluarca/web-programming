7676043dc4c7527dc724a8f80de12283cdcdbc14
Juan <juanjluarca@gmail.com>
2025-09-07 15:33:39 -0600

    feat: creating book model

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Author
 from .models import Country
+from .models import Book
 # Register your models here.
 
 @admin.register(Author)
@@ -11,3 +12,6 @@ class AuthorAdmin(admin.ModelAdmin):
 class CountryAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
--- library/models.py
+++ library/models.py
@@ -9,6 +9,13 @@ class Author(models.Model):
     country = models.ForeignKey('Country', on_delete=models.SET_NULL, null=True, blank=True)
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=75)
+    publication_date = models.DateField()
+    pages = models.PositiveIntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
 class Country(models.Model):
     name = models.CharField(max_length=50)
     population = models.PositiveBigIntegerField()
